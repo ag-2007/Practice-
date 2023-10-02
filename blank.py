566cdc5749d37542afc763bcfc13a2f1187e935e
Anika Gupta <20anika07@gmail.com>
2023-10-02 21:48:22 +0530

    first commit

---
+++ blank.py
@@ -0,0 +1,10 @@
+# Write your code here :-)
+print ('Hello World')
+print ('What is your name')
+myName = input()
+print ('it is good to meet you ' + myName)
+print ('How old are you')
+myAge = input()
+print ('did you know you will be be ' + str(int(myAge)+1) + ' in a year?')
+
+
