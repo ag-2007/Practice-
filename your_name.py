80af8abe8d2901f77678f9855ba1b2e6eea9193b
ag-2007 <145470362+ag-2007@users.noreply.github.com>
2023-10-03 22:16:17 +0530

    Add files via upload

---
+++ your_name.py
@@ -0,0 +1,9 @@
+# Write your code here :-)
+print ('Please type your name')
+name= input()
+while name != 'your name':
+    print ('please type you name')
+    name = input()
+print ('Thank you')
+
+
