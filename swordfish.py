a801917f2f8dc7ee98bf1c4c2d11a9d9be48a8f8
ag-2007 <145470362+ag-2007@users.noreply.github.com>
2023-10-03 22:15:56 +0530

    Add files via upload

---
+++ swordfish.py
@@ -0,0 +1,11 @@
+# Write your code here :-)
+while True:
+    print ('Who are you')
+    name= input()
+    if name != 'Anika':
+        continue
+    print ('Hello Anika, What is the password?')
+    password= input()
+    if password=='swordfish':
+         break
+print ('Access Granted')
