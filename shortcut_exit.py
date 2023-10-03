bc16689d7eddd89511e2e3edeb5efed135744712
ag-2007 <145470362+ag-2007@users.noreply.github.com>
2023-10-03 22:14:52 +0530

    Add files via upload

---
+++ shortcut_exit.py
@@ -0,0 +1,8 @@
+# Write your code here :-)
+import sys
+while True:
+    print ('type exit to exit')
+    response=input()
+    if response=='exit':
+        sys.exit()
+    print ('you typed ' + response)
