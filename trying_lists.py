df1d6b0943ff0231423ddb4c85a1f6a9cd85d63d
ag-2007 <145470362+ag-2007@users.noreply.github.com>
2023-10-07 11:15:12 +0530

    Add files via upload

---
+++ trying_lists.py
@@ -0,0 +1,12 @@
+spam=["cat","bat","elephant","ostrich"]
+#Trying out different operations
+print (spam[1])
+print ('Hello '+ spam[3] + ", over here")
+print(spam[0:3])
+print(spam[:])
+print (len(spam))
+
+spam = spam + ["lion","tiger"]
+print (spam)
+del spam[5]
+print (spam)
