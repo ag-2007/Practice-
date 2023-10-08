bd55ee2e237c42432d23e54f8354ad6fb6dc67a8
ag-2007 <145470362+ag-2007@users.noreply.github.com>
2023-10-08 10:04:11 +0530

    Add files via upload

---
+++ prescence.py
@@ -0,0 +1,7 @@
+mySubjects = ['Physics','Chemistry','Math','English','French','Computer Science']
+print ('Enter a Subject that has an upcoming test')
+Subject=input()
+if Subject in mySubjects:
+    print ('You are studying this subject, be worried')
+else:
+    print ('Chill, you are not studying this subject')
