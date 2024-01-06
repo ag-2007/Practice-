955d08755b8652e4e75f86a1d24d8d700a25c69e
ag-2007 <145470362+ag-2007@users.noreply.github.com>
2024-01-06 14:17:16 +0530

    Update birthday_dictionary.py

--- birthday_dictionary.py
+++ birthday_dictionary.py
@@ -1,6 +1,6 @@
-birthdays={"Anika":"20/07/2007", "Jake":"09/07/2008","Jill":"03/03/2006"}
+birthdays={"anika":"20/07/2007", "jake":"09/07/2008","jill":"03/03/2006"}
 print("Welcome to the birthday dictionary! We know the birthdays of :")
 for key in birthdays.keys():
-    print(key)
-request=input("Whose birthday would you like to know : ")
-print(f"{request}'s birthday is on {birthdays[request]}")
+    print(key.capitalize())
+request=input("Whose birthday would you like to know : ").lower()
+print(f"{request.capitalize()}'s birthday is on {birthdays[request]}")
