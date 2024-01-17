6a5ce28e42df2cb4a5d6e4bd2bef4d640456bed4
ag-2007 <145470362+ag-2007@users.noreply.github.com>
2024-01-17 12:52:46 +0530

    Update coin_flip.py

--- coin_flip.py
+++ coin_flip.py
@@ -10,5 +10,12 @@ for experimentNumber in range(10000):
 
 
     # Code that checks if there is a streak of 6 heads or tails in a row.
+    streak_count = 0
+    for i in range(len(coin_flips) - 5):
+        if all(coin_flips[i + j] == coin_flips[i] for j in range(6)):
+            streak_count += 1
+
+    if streak_count > 0:
+        numberOfStreaks += 1
     
 print('Chance of streak: %s%%' % (numberOfStreaks / 100))
