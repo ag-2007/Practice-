c593015318123fb990448586f1fc64a15fd45a1b
ag-2007 <145470362+ag-2007@users.noreply.github.com>
2024-02-15 02:43:34 +0530

    Add files via upload

---
+++ numpy_p7_rand_ops.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+##Code commented out with one hash to see individual results
+
+#print(np.random.random(1))
+#print(np.random.random(2))
+#print(np.random.random((2,2)))
+#print(np.random.randint(1,10))
+#print(np.random.randint(1,10,(2,2)))
+#print(np.random.randint(1,10,(3,4,5)))
+
+##rand is for floats and randn is for negative floats
+#print(np.random.rand(2,2))
+#print(np.random.randn(2,2))
+
+a = np.arange(1,10)
+print(a)
+
+print(np.random.choice(a))
+
+
+
